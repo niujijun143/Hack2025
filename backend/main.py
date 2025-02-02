6e47aac3f399310ff02c5af664ae00f8c688844e
jaivatsan-elango-sundaram <je.sundaram@gmail.com>
2025-02-02 00:06:52 -0500

    fixed the methods

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,17 @@
 from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = "HACKHACK"
+from auth import auth
+from views import views
+# def create_app():
+#     app = Flask(__name__)
+#
+#     app.register_blueprint(auth, url_prefix='/')
+#     app.register_blueprint(views, url_prefix='/')
+#     return app
     
-    from auth import auth 
-    from views import views
-
-    app.register_blueprint(auth, url_prefix='/')
-    app.register_blueprint(views, url_prefix='/')
-    return app
-    
-app = create_app()
+# app = create_app()
+app = Flask(__name__)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
 
--- backend/main2.py
+++ backend/main2.py
@@ -1,14 +1,31 @@
+from flask import Flask
+from auth import auth
+from views import views
+
 from flask import jsonify
 from pymongo import MongoClient
 from datetime import datetime
-from main import app
+# def create_app():
+#     app = Flask(__name__)
+#
+#     app.register_blueprint(auth, url_prefix='/')
+#     app.register_blueprint(views, url_prefix='/')
+#     return app
+
+# app = create_app()
+app = Flask(__name__)
+
+
+# from main import app
 client = MongoClient('mongodb+srv://db123:db123@violethacks.o5ozy.mongodb.net/')
 # Define databases
 record_db = client['record_db']
 expenses_db = client['expenses_db']
 # id_ex = 0
 # new_expense_id = 0
-@app.route('/createTrip/<int: user_id>,')
+
+
+@app.route('/createTripEvent/<int:user_id>/<string:trip_name>/<string:trip_start>/<string:trip_end>/<string:currency>/<float:exchange>', methods=['POST'])
 def create_trip_event(user_id, trip_name, trip_start, trip_end, currency, exchange):
     try:
         """
@@ -38,9 +55,10 @@ def create_trip_event(user_id, trip_name, trip_start, trip_end, currency, exchan
     except:
         return jsonify({'error': 'Item not found'}), 404
     else:
+            #this is a post method y are we returning anything
             #return new_expense_id, 200
             return jsonify({'error': 'works'}), 200
-@app.route('/createExp')
+@app.route('/createExp/<int:user_id>/<int:id_ex>/<float:amt_h>/<float:amt_f>/<string:cat>/<string:notes>', methods=['POST'])
 def create_tripExp(user_id, id_ex, amt_h, amt_f, cat, notes):
     try:
         """
@@ -76,9 +94,11 @@ def create_tripExp(user_id, id_ex, amt_h, amt_f, cat, notes):
     except:
         return jsonify({'error': 'Item not found'}), 404
     else:
+            #post method, do we need to return aything
             #return new_expense_id, 200
             return jsonify({'error': 'works'}), 200
-@app.route('/<int: user_id>')
+
+@app.route('/getTrips/<int:user_id>', methods=['GET'])
 def get_user_trips(user_id):
     try:
         """
@@ -88,18 +108,32 @@ def get_user_trips(user_id):
 
         user_trips = list(records.find({'user_id': user_id}, {'_id': 0}))
     except:
-        return jsonify({'error': 'Item  found'}), 404
+        return jsonify({'error': 'Item not found'}), 404
     else:
         #return user_trips if user_trips else "No trips found for this user.", 200
-        return jsonify({'error': 'works'}), 200
+        return jsonify({'no error': 'works'}), 200
 
-@app.route('/deleteTrip')
+@app.route('/deleteTrip/<user_id>/<trip_name>/<trip_start>/<trip_end>/<currency>/<exchange>', methods=['DELETE'])
 def delete_record(user_id, trip_name, trip_start, trip_end, currency, exchange):
-      record_db.delete_one(user_id, trip_name, trip_start, trip_end, currency, exchange)
+      try:
+        records = record_db['records']
+        query = {  # Create a dictionary for the query
+            'user_id': user_id,
+            'trip_name': trip_name,
+            'trip_start': trip_start,
+            'trip_end': trip_end,
+            'currency': currency,
+            'exchange': exchange
+        }
+        result = records.delete_one(query)
+        #record_db.delete_one(user_id, trip_name, trip_start, trip_end, currency, exchange)
+      except:
+          return jsonify({'error': 'Item not found'}), 404
+      else:
+          #deleting, nothing to return
+          return jsonify({'no error': 'works'}), 200
 
-@app.route("/favicon.ico")
-def favicon():
-    return "", 200
+
 # Example Usage
 #
 # id_ex = create_trip_event(2, "Japan", "2024-01-02", "2024-01-10", "yan", 7.14)
@@ -109,3 +143,6 @@ def favicon():
 # # Fetch all trips for user_id 2
 # user_trips = get_user_trips(2)
 # print(user_trips)
+if __name__ == '__main__':
+    app.run(debug=True)
+
